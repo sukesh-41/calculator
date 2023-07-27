1be1f394746e7bf033f16f04d3945b84d2e880cc
sukesh-41 <136302771+sukesh-41@users.noreply.github.com>
2023-07-27 21:55:09 +0530

    Create calc.js

---
+++ calc.js
@@ -0,0 +1,9 @@
+function calculate() {
+    calculateform.screen.value = eval(calculateform.screen.value);
+}
+function valuebutton(numb) {
+    calculateform.screen.value += numb.value;
+}
+function clear1() {
+    calculateform.screen.value = null;
+}
